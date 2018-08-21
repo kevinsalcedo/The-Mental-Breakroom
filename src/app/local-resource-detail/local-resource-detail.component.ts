3380300fa4bc04fc59e3cdb36428a9992754501d
Kevin Salcedo <kevsalcedo@gmail.com>
2018-08-20 19:13:22 -0500

    added local resource detail

--- src/app/local-resource-detail/local-resource-detail.component.ts
+++ src/app/local-resource-detail/local-resource-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-local-resource-detail',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./local-resource-detail.component.css']
 })
 export class LocalResourceDetailComponent implements OnInit {
+  resourceId = '-1';
+  disorderId = '-1';
+  private resource: object = {};
+  private disorder: object = {};
 
-  constructor() { }
+  constructor(private urlPath: Location, private apiService:ApiService) { }
 
   ngOnInit() {
+    this.resourceId = this.urlPath.path().replace('/localresources/','');
+    this.getLocalResource(this.resourceId);
   }
 
+  public getLocalResource(resourceId) {
+    this.apiService.getLocalResource(this.resourceId).subscribe((resourceData: Array<object>) => {
+      this.resource = resourceData;
+      this.disorderId = resourceData['disorder']
+      this.getDisorder(this.disorderId);
+    });
+  }
+
+  public getDisorder(disorderId) {
+    this.apiService.getDisorder(this.disorderId).subscribe((data: Array<object>) => {
+      this.disorder = data;
+    });
+  }
 }
